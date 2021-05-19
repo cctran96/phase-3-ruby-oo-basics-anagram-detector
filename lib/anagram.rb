0d69862ef26cc709e49a8254ec77f271059c96fc
Chau Tran <cctran96@gmail.com>
2021-05-18 23:02:58 -0700

    Done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,11 @@
-# Your code goes here!
+class Anagram
+    def initialize(word)
+        @word = word
+    end
+
+    def match(words)
+        arr = []
+        words.each{|w| w.split("").sort == @word.split("").sort ? arr << w : nil}
+        arr
+    end
+end
